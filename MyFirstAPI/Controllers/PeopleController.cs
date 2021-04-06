df923a071e46aad44314a5ed4d1104fc49d3c01d
jordan.woodruff <jordan.woodruff@tylertech.com>
2021-04-05 19:53:52 -0500

    MyFirstAPI

---
+++ MyFirstAPI/Controllers/PeopleController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PeopleController : ControllerBase
+    {
+        private readonly List<Person> _people = new List<Person>
+        {
+            new Person
+            {
+                Id = 1,
+                Name = "Luke Skywalker",
+                HairColor = "blond"
+            },
+            new Person
+            {
+                Id = 2,
+                Name = "Leia Organa",
+                HairColor = "brown"
+            },
+            new Person
+            {
+                Id = 3,
+                Name = "Han Solo",
+                HairColor = "brown"
+            }
+        };
+        
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_people);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var person = _people.FirstOrDefault(p => p.Id == id);
+
+            if (person == null)
+                return NotFound();
+
+            return Ok(person);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Person person)
+        {
+            person.Id = _people.Count + 1;
+
+            return CreatedAtAction("Post", person);
+        }
+    }
+}
---
+++ MyFirstAPI/Controllers/ValuesController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        private readonly string[] _values = new[] {"value1", "value2", "value3"};
+        
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return _values;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            id--;
+
+            if (id > 2)
+                return NotFound();
+
+            return Ok(_values[id]);
+        }
+    }
+}
